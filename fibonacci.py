a00a39b2c87c5c4f7b0a57264da43a273b7fbbb8
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2022-07-11 16:19:15 +0530

    fibonacci added

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+prev0 = 0
+prev1 = 0
+for x in range(25):
+  if (prev0 == 0 and prev1 == 0):
+    prev1 = 0
+    prev0 = 1
+  else:
+    tmp = prev1
+    prev1 = prev0 + prev1
+    prev0 = tmp
+  print(prev0 + prev1)
