a30fa6dd7b9876e862f33094ad5ef32951274ab3
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2022-07-27 21:48:06 +0530

    made it light wait

--- lock.py
+++ lock.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 import os
 
 now = datetime.datetime.now()
@@ -7,3 +8,4 @@ while True:
     if now.hour > 18 and now.hour < 7:
         os.system('shutdown -s -t 120 /c "shutting down in 2 minutes! Make sure to save your work, see ya later aligator!"')
         break
+    time.sleep(.01)
