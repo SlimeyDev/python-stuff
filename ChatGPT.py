c90a54930c519bd73374f9e73b92ca5faecae3fb
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2023-04-08 00:38:37 +0530

    e

--- ChatGPT.py
+++ ChatGPT.py
@@ -2,14 +2,5 @@ import openai
 
 openai.api_key = "####"
 
-messages = []
-system_msg = "chat"
-messages.append({"role": "system", "content": system_msg})
-message = input()
-messages.append({"role": "user", "content": message})
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages)
-reply = response["choices"][0]["message"]["content"]
-messages.append({"role": "assistant", "content": reply})
-print("\n" + reply + "\n")
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "hello!"}])
+print(completion.choices[0].message.content)
