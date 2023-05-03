190c26746a337ad1d928f6a9d077ec8b859d28b8
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2023-05-03 13:41:24 +0530

    small update

--- notepadTest.py
+++ notepadTest.py
@@ -1,8 +1,3 @@
-def main():
-    f= open("test.txt","w+")
-
-    for i in range(10):
-        f.write("hey"+"yo"+"\n")
-
-if __name__=="__main__":
-    main()
+f= open("test.txt","w+")
+for i in range(10):
+    f.write("hey"+"yo"+"\n")
